6e62c6673d5b26d3565c053d47b1e63a59c2b612
Davi <daviribeirotesta@gmail.com>
2023-01-09 01:37:36 -0300

    unauthorized funcionando

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
-import express, {json} from "express";
+import express, { json } from "express";
 import cors from "cors";
 
 const server = express();
 server.use(cors());
 server.use(json());
 
+const users = [];
+const tweets = [];
+
+server.post("/sign-up", (req, res) => {
+    const { username, avatar } = (req.body);
+    const userBody = { username, avatar }
+    users.push(userBody);
+    res.send("OK")
+})
+
+server.get("/sign-up", (req, res) => {
+    res.send(users);
+});
+
+server.post("/tweets", (req, res) => {
+    const { username, tweet } = (req.body)
+    const { avatar } = users.find(user => user.username == username);
+    const tweetBody = { username, tweet, avatar }
+    const login = users.find(user => user.username === "")
+
+    if (login) {
+        return res.status(401).send("UNAUTHORIZED");
+    }
+    else {
+        tweets.push(tweetBody)
+        return res.send("OK")
+    }
+})
+
+server.get("/tweets", (req, res) => {
+    res.send(tweets.slice(-10).reverse());
+});
+
+
+const PORT = 5000;
+
+server.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
